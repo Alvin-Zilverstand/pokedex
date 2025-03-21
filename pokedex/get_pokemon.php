afcd8d5b7cfbcad2708065ed453e941d15222026
vista-man <524715@vistacollege.nl>
2025-03-21 13:40:54 +0100

    Add database schema for Pokémon and update get_pokemon.php to include Japanese name

--- pokedex/get_pokemon.php
+++ pokedex/get_pokemon.php
@@ -15,7 +15,7 @@ if ($conn->connect_error) {
 if (isset($_GET['id'])) {
     $id = intval($_GET['id']);
 
-    $sql = "SELECT p.id, p.name, p.height, p.weight, p.base_experience, p.image_url, s.genus, s.flavor_text, s.growth_rate, s.base_happiness, s.capture_rate, s.gender_rate, GROUP_CONCAT(DISTINCT t.name) AS types, GROUP_CONCAT(DISTINCT a.name) AS abilities, GROUP_CONCAT(DISTINCT e.name) AS egg_groups, st.hp, st.attack, st.defense, st.sp_attack, st.sp_defense, st.speed
+    $sql = "SELECT p.id, p.name, p.height, p.weight, p.base_experience, p.image_url, s.genus AS japanese_name, s.flavor_text, s.growth_rate, s.base_happiness, s.capture_rate, s.gender_rate, GROUP_CONCAT(DISTINCT t.name) AS types, GROUP_CONCAT(DISTINCT a.name) AS abilities, GROUP_CONCAT(DISTINCT e.name) AS egg_groups, st.hp, st.attack, st.defense, st.sp_attack, st.sp_defense, st.speed
             FROM pokemon p
             LEFT JOIN species s ON p.id = s.pokemon_id
             LEFT JOIN pokemon_types pt ON p.id = pt.pokemon_id
