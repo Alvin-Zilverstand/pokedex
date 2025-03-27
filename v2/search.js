d5a242e7ea7a5e280f55ec239451ee124ed88e57
vista-man <524715@vistacollege.nl>
2025-03-27 09:08:41 +0100

    Improve search functionality by encapsulating event listeners in an IIFE and enhancing visibility toggle for the search close icon

--- v2/search.js
+++ v2/search.js
@@ -1,38 +1,40 @@
-const inputElement = document.querySelector("#search-input");
-const searchIcon = document.querySelector("#search-close-icon");
-const sortWrapper = document.querySelector(".sort-wrapper");
+(() => {
+  const inputElement = document.querySelector("#search-input");
+  const searchIcon = document.querySelector("#search-close-icon");
+  const sortWrapper = document.querySelector(".sort-wrapper");
 
-inputElement.addEventListener("input", () => {
-  handleInputChange(inputElement);
-});
-searchIcon.addEventListener("click", handleSearchCloseOnClick);
-sortWrapper.addEventListener("click", handleSortIconOnClick);
+  inputElement.addEventListener("input", () => {
+    handleInputChange(inputElement);
+  });
+  searchIcon.addEventListener("click", handleSearchCloseOnClick);
+  sortWrapper.addEventListener("click", handleSortIconOnClick);
 
-function handleInputChange(inputElement) {
-  const inputValue = inputElement.value;
+  function handleInputChange(inputElement) {
+    const inputValue = inputElement.value;
 
-  if (inputValue !== "") {
-    document
-      .querySelector("#search-close-icon")
-      .classList.add("search-close-icon-visible");
-  } else {
+    if (inputValue !== "") {
+      document
+        .querySelector("#search-close-icon")
+        .classList.add("search-close-icon-visible");
+    } else {
+      document
+        .querySelector("#search-close-icon")
+        .classList.remove("search-close-icon-visible");
+    }
+  }
+
+  function handleSearchCloseOnClick() {
+    document.querySelector("#search-input").value = "";
     document
       .querySelector("#search-close-icon")
       .classList.remove("search-close-icon-visible");
+    clearSearch();
   }
-}
 
-function handleSearchCloseOnClick() {
-  document.querySelector("#search-input").value = "";
-  document
-    .querySelector("#search-close-icon")
-    .classList.remove("search-close-icon-visible");
-  clearSearch();
-}
-
-function handleSortIconOnClick() {
-  document
-    .querySelector(".filter-wrapper")
-    .classList.toggle("filter-wrapper-open");
-  document.querySelector("body").classList.toggle("filter-wrapper-overlay");
-}
+  function handleSortIconOnClick() {
+    document
+      .querySelector(".filter-wrapper")
+      .classList.toggle("filter-wrapper-open");
+    document.querySelector("body").classList.toggle("filter-wrapper-overlay");
+  }
+})();
